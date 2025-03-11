6ce3414e1a02b21408dd33883a579e0915406686
Priyesh <Priyeshwas11@gmail.com>
2025-03-11 10:58:51 +0530

    learned datatype conversion

---
+++ JavaScript/03_datatype-conversion.js
@@ -0,0 +1,42 @@
+// let moon="11";
+// console.log(typeof (moon)); //string
+// let moonInNum = Number(moon); //11
+// console.log(typeof (moonInNum)); //number
+
+// let moon1="11ab";
+// console.log(typeof (moon1)); //string
+// let moonInNum1 = Number(moon1); //11ab => NaN
+// console.log(typeof (moonInNum1)); //number
+// console.log(moonInNum1); //NaN => Not a Number
+
+// let moon2= null; //null
+// console.log(typeof (moon2)); //object
+// let moonInNum2 = Number(moon2); //null => 0
+// console.log(typeof (moonInNum2)); //number
+// console.log(moonInNum2); //0
+
+
+// let moon3= true; //true
+// console.log(typeof (moon3)); //boolean
+// let moonInNum3 = Number(moon3); //true => 1, false => 0
+// console.log(typeof (moonInNum3));  //number
+// console.log(moonInNum3); //1
+
+
+
+let IsLoggedIn = 1;
+let bool = Boolean(IsLoggedIn);
+console.log(bool); //true
+
+// 1 => true ; 0 => false
+// "" => false ; " " => true
+// "priyesh" => true ; " " => true
+// null => false ; undefined => false
+
+let something = 69; //number
+let StringSomething = String(something); 
+console.log(typeof StringSomething); //string
+console.log(StringSomething); //69
+
+
+
