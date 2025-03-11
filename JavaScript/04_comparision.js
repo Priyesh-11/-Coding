a0c9146f094935ff1fefabeeb72f4d8c9db3ce6d
Priyesh <Priyeshwas11@gmail.com>
2025-03-11 22:50:56 +0530

    add comparison examples in JavaScript

---
+++ JavaScript/04_comparision.js
@@ -0,0 +1,25 @@
+// console.log(7 > 5); // true
+// console.log(7 < 5); // false
+// console.log(7 >= 5); // true
+// console.log(7 <= 5); // false   
+// console.log(7 == 5); // false
+// console.log(7 != 5); // true
+// console.log(7 === 5); // false
+// console.log(7 !== 5); // true
+
+
+// console.log("7">5); // true
+
+// console.log(null>0); // false because null is converted to 0
+// console.log(null==0); // false because null is not converted to 0
+// console.log(null>=0); // true because null is converted to 0
+
+// console.log(undefined>0); // false because undefined is converted to NaN
+// console.log(undefined==0); // false because undefined is not converted to 0
+// console.log(undefined>=0); // false because undefined is converted to NaN
+
+// console.log(undefined==null); // true
+// console.log(undefined===null); // false
+
+// console.log("7"==7); // true
+// console.log("7"===7); // false
