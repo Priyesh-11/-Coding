ec7e099e7c88da162e4a5b09f6a4c2e6b08130de
Priyesh <priyeshwas11@gmail.com>
2025-06-01 13:03:14 +0530

    refactor date handling in JavaScript, enhancing output formatting and clarity

--- JavaScript/08_date&time.js
+++ JavaScript/08_date&time.js
@@ -5,7 +5,30 @@ let mydate = new Date();
 // console.log(mydate.toLocaleDateString());
 
 // let Adate = new Date(2024,0,11)
-// let Adate = new Date("2024-1-11")
+let Bdate = new Date("2024-1-11")
 let Adate = new Date("11-01-2024-07:11:09-00:00");
 
-console.log(Adate.toLocaleString());
+// console.log(Adate.toLocaleString());
+
+let timestamp = Date.now();
+// console.log(Bdate.getTime());
+
+// console.log(Adate.getMinutes()); //
+// console.log(Adate.getDay()); // 
+// console.log(Adate.getMonth() + 1); // January is 0
+// console.log(Adate.getFullYear()); // 2024
+
+
+Adate.toLocaleString('default', {
+
+    weekday: "long"
+})
+console.log(Adate.toLocaleString('default', {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit"
+})); // Friday, January 11, 2024, 07:11:09 AM
