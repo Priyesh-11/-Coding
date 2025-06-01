fe7c47ed2cfadd91dcc520cd769ddc4b5a8cecb2
Priyesh <priyeshwas11@gmail.com>
2025-06-01 12:33:38 +0530

    add date and time handling examples in JavaScript

---
+++ JavaScript/08_date&time.js
@@ -0,0 +1,11 @@
+// dates
+let mydate = new Date();
+// console.log(mydate.toString());
+// console.log(mydate.toTimeString());
+// console.log(mydate.toLocaleDateString());
+
+// let Adate = new Date(2024,0,11)
+// let Adate = new Date("2024-1-11")
+let Adate = new Date("11-01-2024-07:11:09-00:00");
+
+console.log(Adate.toLocaleString());
