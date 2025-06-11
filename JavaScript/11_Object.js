07b5dda56df37161416bfdfec932fa8fe60c43c6
Priyesh <priyeshwas11@gmail.com>
2025-06-11 18:09:52 +0530

    update object properties and add fullname to info object in JavaScript

---
+++ JavaScript/11_Object.js
@@ -0,0 +1,23 @@
+let mysym = Symbol("key"); // Creating a symbol
+
+const info = {
+  name: "Priyesh",
+  "full name": "Priyesh Singh",
+  age: 32,
+  [mysym]: "2key",
+  email: "priyeshwas11@gmail.com",
+  isStudent: true,
+  subjects: ["JavaScript", "Python", "Java"],
+};
+
+info.name = "Priyesh Was"; //
+info["age"] = 33; // Updating properties
+// info.fullname = "Priyesh Was"; // This will not work as 'fullname' is not defined in the object
+info["fullname"] = "Priyesh Singh"; // Correct way to add a new property
+// console.log(info.name); // Priyesh Was
+// console.log(info["age"]); // 33
+
+// console.log(info[mysym]); // 2key
+
+// console.log(info);
+
